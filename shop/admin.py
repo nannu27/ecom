9163e79c41e528cee367569d6fc8acc94466213d
nannu27 <46987940+nannu27@users.noreply.github.com>
2020-11-27 21:43:45 +0530

    changes info

---
+++ shop/admin.py
@@ -0,0 +1,6 @@
+from .models import Product, Contact, Orders,OrderUpdate
+from django.contrib import admin
+admin.site.register(Product)
+admin.site.register(Contact)
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
