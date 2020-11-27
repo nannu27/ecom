12afec1feea4a88c2384a6bd2d5ca0d7f12d1698
nannu27 <46987940+nannu27@users.noreply.github.com>
2020-11-27 23:57:31 +0530

    Update wsgi.py

--- ecom/wsgi.py
+++ ecom/wsgi.py
@@ -10,10 +10,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecom.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 
-application = DjangoWhiteNoise(application)
