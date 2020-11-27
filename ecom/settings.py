24a52ff66aa0c3ce94c6e8cc5ada75c14f6210dc
nannu27 <46987940+nannu27@users.noreply.github.com>
2020-11-27 22:22:05 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'd%awn949krs#cf7)m$y0#e4ug=w*bgc^khxuk@w*s25ec^#=x#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sscollection1.herokuapp.com']
 
 
 # Application definition
